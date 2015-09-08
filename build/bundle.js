dc22b3c7bb088ae5604d340fb72a7aae7ac5b902
Meng Chen <tamulemon@gmail.com>
2015-09-07 21:10:08 -0700

    config accessToken

--- build/bundle.js
+++ build/bundle.js
@@ -1048,6 +1048,7 @@ var MapContainer = module.exports = React.createClass({displayName: "exports",
 			.end(function(err, res) {
 			if (res.ok) {
 				parseString(res.text, function(err, result) {
+					
 					var outputArr = result['RegionChildren:regionchildren']['response'][0]['list'][0]['region'];
 					var medianObj = {},
 							name = '',
@@ -1120,132 +1121,136 @@ var MapContainer = module.exports = React.createClass({displayName: "exports",
 
 	// all layers are placed when component is mounted
 	componentDidMount: function() {
-
-		this.loadAllNeighborhoods();
+		
 		this.getAccessToken();
-		console.log(accessToken);
+		this.loadAllNeighborhoods();
+		
+		
 	// initiate map layer. Because map is not a DOM element, it doesn't get refreshed so shouldn't live on this.state
-		var map = this.map = L.map('map', {
-								center: [47.609, -122.250],
-								zoom: 12,
-								minZoom: 2,
-								maxZoom: 13,
-								layers: [
-									L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + accessToken, {
-									attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-									id: 'mobot11.1dba3612'
-								})
-							],
-								attributionControl: false,
-					});
 
-	// add attribution
-		var attribution = L.control({position: 'bottomright'});
-		attribution.onAdd = function(map) {
-			var div = L.DomUtil.create('div', 'attribution');
-			div.innerHTML = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>;  Crime data &copy 2014 City of Seattle;  Neighborhood data &copy Zillows <img  height="15" src="http://www.zillowstatic.com/vstatic/fb9712c/static/logos/Zillow_Logo_HoodsProvided_RightAligned.gif">';
-			return div;
-		};
-		attribution.addTo(map);
 
 		
-	// add legend for choradepth graph
-		var legend = L.control({position: 'bottomleft'});
-		legend.onAdd = function (map) {
-			var div = L.DomUtil.create('div', 'info legend'),
-					grades = [200000, 300000, 400000, 500000, 600000, 700000, 1000000],
-					gradesLegend = ['200k', '300k', '400k', '500k', '600k', '700k','1M', 'No data'],
-
-					labels = [];
-			div.innerHTML = '<h5 margin="0">Zestimate Home Prices</h5>'
-			for (var i = 0; i < grades.length; i++) {
-				div.innerHTML +=
-					'<i style="background:' + this.getColor(grades[i] + 1) + '"></i> ' +
-					gradesLegend[i] + (i < grades.length - 1 ? '&ndash;' + gradesLegend[i + 1] + '<br>' : '+' + '<br>');
-			}
-			div.innerHTML += '<i style="background:' + this.getColor(-1) + '"></i> ' + gradesLegend[i];
-			return div;
-		}.bind(this);
-		legend.addTo(map);
-
-		
-	// add a control that display neighborhood name when hovered over
-		var info = L.control({position: 'topleft'});
-		//when add is called, dom will create a div with class info. This is DOM element so an be CSS styled 
-		info.onAdd = function(map) {
-			this._div = L.DomUtil.create('div', 'info');
-			return this._div;
-		};
-		info.update = function(name) {
-			this._div.innerHTML = (name ? '<b>' + name + '</b><br />' : 'Hover over a neighborhood');
-		};
-		info.addTo(map);
-		
-		
-	// add markerlayer to map
-		
-		// park data
-		var parkIcon = L.icon({
-			iconUrl: '../data/pine-48.png',
-			iconSize: [15, 25],
-			iconAnchor: [22, 49],
-			popupAnchor: [-3, -55] // point from which the popup should open relative to the iconAnchor
-		});
-
-		var parkMarkers= parkData.map(function(arr) {
-					if (!arr[3]) {
-						return L.marker([arr[1], arr[0]], {icon: parkIcon}).bindPopup(arr[2]);
-					}
-					else {
-						return L.marker([arr[1], arr[0]], {icon: parkIcon}).bindPopup(arr[2] + arr[3]);
-					}
-				});
-
-		var newMarkers = L.featureGroup(parkMarkers);
-		
-		var showPark = false;
-
-		this.parkClickHandler = function(){
-			if(!showPark) {
-				map.addLayer(newMarkers);
-				showPark= true;
-			}
-			else {
-				map.removeLayer(newMarkers);
-				showPark= false;
-			};
-		};
-
-
-	//			crime button and crime marker functionality
-		var crimeMarkers = crimeData.map(function(arr) {
-			if (!arr[2]) {
-				return L.marker([arr[0], arr[1]]);
-			}
-			else {
-				return	L.marker([arr[0], arr[1]]).bindPopup("date: " + '</br>' + arr[2].slice(0,10));
-			}
-		});
-
-		var crimeMarkers = L.featureGroup(crimeMarkers);
-
-		var showCrime = false;
-
-		this.crimeClickHandler = function(){
-			if(!showCrime) {
-				map.addLayer(crimeMarkers);
-				showCrime= true;
-			}
-			else {
-				map.removeLayer(crimeMarkers);
-				showCrime= false;
-			};
-		};
 
 
 		//add style layer and event listener. delay loading until data is ready because map will not be refreshed when this.state changes
 		setTimeout(function(){
-			
+			var map = this.map = L.map('map', {
+				center: [47.609, -122.250],
+				zoom: 12,
+				minZoom: 2,
+				maxZoom: 13,
+				layers: [
+					L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + accessToken, {
+						attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+						id: 'mobot11.1dba3612'
+					})
+				],
+				attributionControl: false,
+			});
+
+			// add attribution
+			var attribution = L.control({position: 'bottomright'});
+			attribution.onAdd = function(map) {
+				var div = L.DomUtil.create('div', 'attribution');
+				div.innerHTML = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>;  Crime data &copy 2014 City of Seattle;  Neighborhood data &copy Zillows <img  height="15" src="http://www.zillowstatic.com/vstatic/fb9712c/static/logos/Zillow_Logo_HoodsProvided_RightAligned.gif">';
+				return div;
+			};
+			attribution.addTo(map);
+
+
+			// add legend for choradepth graph
+			var legend = L.control({position: 'bottomleft'});
+			legend.onAdd = function (map) {
+				var div = L.DomUtil.create('div', 'info legend'),
+						grades = [200000, 300000, 400000, 500000, 600000, 700000, 1000000],
+						gradesLegend = ['200k', '300k', '400k', '500k', '600k', '700k','1M', 'No data'],
+
+						labels = [];
+				div.innerHTML = '<h5 margin="0">Zestimate Home Prices</h5>'
+				for (var i = 0; i < grades.length; i++) {
+					div.innerHTML +=
+						'<i style="background:' + this.getColor(grades[i] + 1) + '"></i> ' +
+						gradesLegend[i] + (i < grades.length - 1 ? '&ndash;' + gradesLegend[i + 1] + '<br>' : '+' + '<br>');
+				}
+				div.innerHTML += '<i style="background:' + this.getColor(-1) + '"></i> ' + gradesLegend[i];
+				return div;
+			}.bind(this);
+			legend.addTo(map);
+
+
+			// add a control that display neighborhood name when hovered over
+			var info = L.control({position: 'topleft'});
+			//when add is called, dom will create a div with class info. This is DOM element so an be CSS styled 
+			info.onAdd = function(map) {
+				this._div = L.DomUtil.create('div', 'info');
+				return this._div;
+			};
+			info.update = function(name) {
+				this._div.innerHTML = (name ? '<b>' + name + '</b><br />' : 'Hover over a neighborhood');
+			};
+			info.addTo(map);
+
+
+			// add markerlayer to map
+
+			// park data
+			var parkIcon = L.icon({
+				iconUrl: '../data/pine-48.png',
+				iconSize: [15, 25],
+				iconAnchor: [22, 49],
+				popupAnchor: [-3, -55] // point from which the popup should open relative to the iconAnchor
+			});
+
+			var parkMarkers= parkData.map(function(arr) {
+				if (!arr[3]) {
+					return L.marker([arr[1], arr[0]], {icon: parkIcon}).bindPopup(arr[2]);
+				}
+				else {
+					return L.marker([arr[1], arr[0]], {icon: parkIcon}).bindPopup(arr[2] + arr[3]);
+				}
+			});
+
+			var newMarkers = L.featureGroup(parkMarkers);
+
+			var showPark = false;
+
+			this.parkClickHandler = function(){
+				if(!showPark) {
+					map.addLayer(newMarkers);
+					showPark= true;
+				}
+				else {
+					map.removeLayer(newMarkers);
+					showPark= false;
+				};
+			};
+
+
+			//			crime button and crime marker functionality
+			var crimeMarkers = crimeData.map(function(arr) {
+				if (!arr[2]) {
+					return L.marker([arr[0], arr[1]]);
+				}
+				else {
+					return	L.marker([arr[0], arr[1]]).bindPopup("date: " + '</br>' + arr[2].slice(0,10));
+				}
+			});
+
+			var crimeMarkers = L.featureGroup(crimeMarkers);
+
+			var showCrime = false;
+
+			this.crimeClickHandler = function(){
+				if(!showCrime) {
+					map.addLayer(crimeMarkers);
+					showCrime= true;
+				}
+				else {
+					map.removeLayer(crimeMarkers);
+					showCrime= false;
+				};
+			};
+	
 			// using globals to track what have been selected
 			var selected;
 			var previousSelected;
