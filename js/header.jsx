d6c040d1b9b09944a3c4a20961995e4352bdc1f4
Meng Chen <tamulemon@gmail.com>
2015-10-06 13:12:53 -0700

    modify github repo

--- js/header.jsx
+++ js/header.jsx
@@ -5,7 +5,7 @@ var Header = module.exports = React.createClass({
 		return (
 			<header>
         <h1>myNeighborhood</h1>
-  			<a href="https://github.com/gouthamvreddy/my-neighborhood" target="_blank">
+			<a href="https://github.com/tamulemon/my-neighborhood" target="_blank">
           <img src="../data/myhoodlogo.png" />
         </a>
 			</header>
